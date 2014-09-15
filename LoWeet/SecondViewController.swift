59e81b57651090d894aa157a91f7a46552c67719
Katlyn Schwaebe <schwaebek@gmail.com>
2014-09-15 09:11:21 -0400

    Initial Commit

---
+++ LoWeet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LoWeet
+//
+//  Created by Katlyn Schwaebe on 9/15/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
